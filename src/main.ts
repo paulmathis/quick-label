16e86959d2515b313c150d08fc650f17a0037108
Paul Mathis <paul.mathis@gmail.com>
2024-02-25 14:13:31 -0800

    feat: added scaling

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,61 @@
 const input = document.getElementById("input") as HTMLInputElement;
 const preview = document.getElementById("preview") as HTMLDivElement;
 const printButton = document.getElementById("print") as HTMLButtonElement;
+const widthInput = document.getElementById("width") as HTMLInputElement;
+const heightInput = document.getElementById("height") as HTMLInputElement;
+const previewWrapper = document.querySelector(
+  ".preview-wrapper"
+) as HTMLDivElement;
+const previewWrapperWrapper = document.querySelector(
+  ".preview-wrapper-wrapper"
+) as HTMLDivElement;
+
+let width = "51";
+let height = "25";
+
+input.value = "Hello, World!";
+widthInput.value = width;
+heightInput.value = height;
+
+preview.style.width = `${width}mm`;
+preview.style.height = `${height}mm`;
+preview.style.fontFamily = "Arial";
+preview.style.lineHeight = "1";
 
 function setText() {
   textFit(preview, {
     alignHoriz: true,
     alignVert: true,
-    alignVertWithFlexbox: true,
   });
+
+  // scale previewWrapper to fit in previewWrapperWrapper
+  const scale = Math.min(
+    previewWrapperWrapper.clientWidth / previewWrapper.clientWidth,
+    previewWrapperWrapper.clientHeight / previewWrapper.clientHeight
+  );
+  previewWrapper.style.transform = `scale(${scale})`;
+
+  // center previewWrapper in previewWrapperWrapper
+  previewWrapper.style.transformOrigin = "0 center";
 }
+
+window.onresize = () => {
+  setText();
+};
+
+widthInput.addEventListener("input", (event) => {
+  const target = event.target as HTMLInputElement;
+  width = target.value;
+  preview.style.width = `${width}mm`;
+  setText();
+});
+
+heightInput.addEventListener("input", (event) => {
+  const target = event.target as HTMLInputElement;
+  height = target.value;
+  preview.style.height = `${height}mm`;
+  setText();
+});
 
 setText();
 
@@ -33,18 +80,13 @@ function printElem(elem: string) {
     justify-content: center;
     flex-direction: column;
   }
+
   .textFitAlignVertFlex {
     display: flex;
   }
+
   .textFitAlignVertFlex .textFitAlignVert {
     position: static;
-  }
-
-  #preview {
-    width: 51mm;
-    height: 25mm;
-    font-family: Arial;
-    line-height: 1;
   }
 
   html, body {
@@ -53,13 +95,12 @@ function printElem(elem: string) {
   }
 
   @page {
-    /* Remove browser default header (title) and footer (url) */
     margin: 0;
-}
+  }
 
   @media print {
     @page {
-      size: 51mm 25mm;
+      size: ${width}mm ${height}mm;
     }
   }
   
